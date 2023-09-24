b015ed7e412ca3b0ab76f7c00aa7a541408a226f
Marwan Zaky <marouaneezzaky@gmail.com>
2023-09-24 01:21:41 +0100

    getting started plugins

--- getting-started/src/main.rs
+++ getting-started/src/main.rs
@@ -1,15 +1,25 @@
 use bevy::prelude::*;
 
+// Components
 #[derive(Component)]
 struct Name(String);
 
 #[derive(Component)]
 struct Person;
 
+// Plugins
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people));
+    }
+}
+
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, greet_people))    
+        .add_plugins((DefaultPlugins, HelloPlugin))
         .run();
 }
 
